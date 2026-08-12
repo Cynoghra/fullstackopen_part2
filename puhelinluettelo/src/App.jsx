1560d42dbe6c491d3dc3a28c80764a141b45dc8a
Jero Pitkänen <terajero@gmail.com>
2026-08-12 22:15:18 +0300

    2.11 tehty

--- puhelinluettelo/src/App.jsx
+++ puhelinluettelo/src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
+import axios from 'axios'
 // ei tarvitse Personia tuoda tänne, koska Display hoitaa sen
 import Display from './components/Display'
 import PersonForm from './components/PersonForm'
 
 const App = () => {
   // Testi aineisto, joka sisältää muutaman henkilön nimen ja puhelinnumeron
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  
+  // 2.11 muutettu axios hakuun
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
   // Tilat uuden henkilön nimen ja puhelinnumeron tallentamiseen sekä suodattimen hallintaan
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
